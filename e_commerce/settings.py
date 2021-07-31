3cc91af939e0ad27748287213bfd21a32c0ea78e
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-07-31 16:20:04 -0400

    AWS S3 funcionando

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'usuarios',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,13 @@ AUTH_USER_MODEL = 'usuarios.CustomUsuario'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+# Configurações do AWS S3
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
